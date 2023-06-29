5e249da1bdda99d0a7b79704165cf42e285546f5
danielbaquini <danielbaquini@protonmail.com>
2023-06-28 22:40:05 -0300

    Wrapped the opendb request around a promise

---
+++ goals.md
@@ -0,0 +1,10 @@
+# Goals list
+This file will list all the development goals, track progres, suggest tests, documentation drafts, etc...
+
+
+## \[x\] Get indexedb reference through promises
+In this one, we must be able to get an indexeddb inside an async function and use "await".
+
+Test suggestion:
+    Check if the instance is indeed returned
+    Check if the onupgradeneeded is called
--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello world!")
+const openDbPromise = (dbName, dbVersion, onUpgradeNeededCallback = () => { }) => {
+    return new Promise((resolve, reject) => {
+        const dbOpenRequest = window.indexedDB.open(dbName, dbVersion);
+
+        dbOpenRequest.onerror = e => {
+            reject(e)
+        };
+
+        dbOpenRequest.onupgradeneeded = onUpgradeNeededCallback
+
+        dbOpenRequest.onsuccess = e => {
+            resolve(e)
+        };
+    })
+}
+
+async function init() {
+    const openDbRequest = await openDbPromise("test", 1);
+    console.log(openDbRequest.target.result)
+}
+
+init()
